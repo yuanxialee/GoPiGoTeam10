b21f6bd46fef805b1b22cdb0bb3e50057dd8faab
Yuanxia Lee <yuanxia.lee@gmail.com>
2017-06-19 16:17:44 -0700

    Working version

--- lab3/navigate.py
+++ lab3/navigate.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 	        for i in range(len(data)):
 	            info.append(float(data[i].strip('\n')))
 
-	gopigo = rover(info[0], info[1])
+	gopigo = rover(info[0], info[1], info[2], info[3])
 	start.extend((info[0],info[1]))
 	goal.extend((info[2],info[3]))
 	dim.extend((info[4],info[5]))
@@ -61,9 +61,9 @@ if __name__ == '__main__':
 		points = obs.get_gobstacle(i)
 		corners = obs.get_shape(i)
 		polygon = np.array(points)
-		plt.plot(polygon[:,0], polygon[:,1],'ko', label = 'grown obstacle')
+		#plt.plot(polygon[:,0], polygon[:,1],'ko', label = 'grown obstacle')
 		original = np.array(corners)
-		plt.plot(original[:,0], original[:,1], 'b', label = 'original obstacle')
+		#plt.plot(original[:,0], original[:,1], 'b', label = 'original obstacle')
 		ch = np.array(convex_hull(points))
 		#print ch
 		k = len(ch)
@@ -73,15 +73,15 @@ if __name__ == '__main__':
 			l += 1
 		gobs.add_block(b, 0)
 		b = block()
-		plt.plot(ch[:,0], ch[:,1],'r--',label = 'convex hull')
+		#plt.plot(ch[:,0], ch[:,1],'r--',label = 'convex hull')
 		i+=1
 		j-=1
 	#print len(gobs.get_obstacles())
-	plt.xlim([0,dim[0]])
-	plt.ylim([0,dim[1]])
-	plt.legend()
-	plt.margins(0.05,0.1)
-	plt.show()
+	#plt.xlim([0,dim[0]])
+	#plt.ylim([0,dim[1]])
+	#plt.legend()
+	#plt.margins(0.05,0.1)
+	#plt.show()
 
 
 	# Visibility graph 
@@ -113,8 +113,9 @@ if __name__ == '__main__':
 	        g.add_edge(start, end-1)
                 block_edges.append(g.get_edge(start, end-1))
                 block_edges.append(g.get_edge(end-1, start))
-        g.visualize(gobs.get_obstacles())
-        plt.show()
+        #g.visualize(gobs.get_obstacles())
+        
+        #plt.show()
         # create eligible path edgs
         for v1 in g.vertices.values():
                 for v2 in g.vertices.values():
@@ -129,18 +130,37 @@ if __name__ == '__main__':
                         if not crossed:
                                 g.add_edge(v1.id, v2.id)
 
-        g.visualize(gobs.get_obstacles())
-        plt.show()
+        #g.visualize(gobs.get_obstacles())
+        #plt.show()
         
         g.dijkstra(0)
         g.visualize(gobs.get_obstacles(), g.path(1))
+        
 
+	path = g.path(1)
+	l = len(path)
+	i = 1
+	gopigo.record_route()
+	while(True):
+	    # state 1: move to goal
+		while l > 1:
+			gopigo.turn_to_goal(path[i][0], path[i][1])
+			i+=1
+			l-=1
+			gopigo.record_route()
+			if gopigo.on_goal():
+			    stop()
+			    gopigo.record_route()
+			    gopigo.state = 'success'
 
+		    # terminate state: either making goal or trapped
 
-
-
-
-        '''
+		print "Current position = ({},{})", gopigo.xpos, gopigo.ypos 
+		if gopigo.state == 'success':
+			print "Success! You found the goal, bug!"
+		break
+	
+	'''
 	i = 1
 	j = 0
 	numv = 1
--- lab3/rover.py
+++ lab3/rover.py
@@ -6,73 +6,69 @@ from pylab import figure, axes, title, show
 from util import *
 
 class rover:
-    path = []					  # record rover location					  
+    route = []					  # record rover location					  
     data = []
 
-    def __init__(self, x, y):
+    def __init__(self, s1, s2, x, y):
         ''' Constructor'''
             # 2D has 3 degree of freedom(DOF)
             # xpos, ypos, and theta are world configuration of robot
         self.state = 'gotogoal'        
-        self.xpos = 0
-        self.ypos = 0
+        self.xpos = s1
+        self.ypos = s2
         self.theta = 0      			  # radians
+
         self.xgoal = x				  # goal coordinates
         self.ygoal = y
-        #self.dgoal = np.arctan(float(y)/float(x)) # target orientation
+        self.dgoal = 0                # target orientation
 
 
     def move(self, dist):
 	'''Move the robot and update position'''
-        set_left_speed(180)
-        set_right_speed(180)
+        set_left_speed(240)
+        set_right_speed(250)
         time.sleep(0.4)
         fwd_cm(dist)
+        time.sleep(4.5)
         self.xpos += dist * np.cos(self.theta)
         self.ypos += dist * np.sin(self.theta)
-        time.sleep(0.4)
+        time.sleep(1)
 
 
     def rotate(self, degree):
 	'''Rotate the robot and update orientation'''
-        set_left_speed(120)
-        set_right_speed(120)
-        time.sleep(WAIT)
+        set_left_speed(150)
+        set_right_speed(160)
+        time.sleep(1)
         rot_deg(degree)
         radian = degree / 180. * np.pi
         self.theta += radian
-        time.sleep(WAIT)
-        set_left_speed(120)
-        set_right_speed(120)
+        time.sleep(1)
+        set_left_speed(150)
+        set_right_speed(160)
 
 
     def on_goal(self):
-        return abs(self.xgoal - self.xpos)+abs(self.ygoal-self.ypos) <= 5.
+        return abs(self.xgoal - self.xpos)+abs(self.ygoal-self.ypos) <= 4.
 
 
-    def turn_to_goal(self):
+    def turn_to_goal(self, x, y):
 	'''
 	Move toward the goal until encountering a hit point or making the goal
 	'''
-        servo(MID)
+        print "Current goal = ({}, {})".format(x, y)
+        self.dgoal = np.arctan(float(y)/float(x))
         diff_rad = self.dgoal - self.theta
         diff_deg = diff_rad/np.pi*180
         print 'diff_rad = ', diff_rad
     	print 'diff_deg = ', diff_deg
+        print "Current position = ({},{})", self.xpos, self.ypos
     	self.rotate(diff_deg)
-
-    	while avg_dist(4) > 30:
-    	    gopigo.record_path()
-    	    print self.report()
-    	    gopigo.move(3)
-    	    time.sleep(0.2)
-    	    if self.on_goal():
-    		break
-
-
-    def dist_to_goal(self, xpos, ypos):
-        return np.sqrt((self.xgoal-xpos)**2+(self.ygoal-ypos)**2)
-
+        time.sleep(WAIT)
+        dist = np.sqrt((x-self.xpos)**2+(y-self.ypos)**2)
+        print "Distance to go = ", dist
+        self.move(dist)
+        time.sleep(1)
 
 
     def report(self):
@@ -83,5 +79,5 @@ class rover:
         print
 
 
-    def record_path(self):
-	   self.path.append((self.xpos,self.ypos))
+    def record_route(self):
+	   self.route.append((self.xpos,self.ypos))
