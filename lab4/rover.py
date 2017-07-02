b3acff283ae1d26ae7d3589beceb8b65a80ab3b0
yc3240 <yc3240@columbia.edu>
2017-07-02 01:38:18 -0400

    update geodesic dilation in dip.py

---
+++ lab4/rover.py
@@ -0,0 +1,44 @@
+from gopigo import *
+from util import *
+from control import *
+
+ZERO = 15
+
+class rover:
+
+	global ZERO	
+
+	def __init__(self, x, y, area):
+		self.cx = x
+		self.cy = y
+		self.carea = area
+
+	def move(self, x, y, area):
+		#while(True):
+		#	if y >= 460:
+		#		print "This is not trackable."
+		#		break
+		if abs(y-self.cy) > 15:
+			if y < self.cy:
+				rot_deg(10)
+				time.sleep(0.3)
+			elif y > self.cy:
+				rot_deg(-10)
+				time.sleep(0.3)
+		if abs(area-self.carea) > ZERO:
+			if area < self.carea:
+				fwd_cm(2)
+				time.sleep(0.2)
+			if area > self.carea:
+				bwd_cm(2)
+				time.sleep(0.2)
+
+	def tolerance(self, area):
+		global ZERO 
+		ZERO = float(area)*(4./43.)+162.
+		print "Area is ", area
+		print "ZERO is ", ZERO
+		if abs(area-self.carea) < ZERO:
+			return True
+		else: 
+			return False 
